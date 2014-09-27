5535aeedf65450ced99a6f5cd637c64d211f275d
Robert <robertwoods2013@gmail.com>
2014-09-27 05:14:59 -0400

    changed to appropriate backend

--- SplashActivity/src/com/quickblox/sample/chat/ui/activities/SplashActivity.java
+++ SplashActivity/src/com/quickblox/sample/chat/ui/activities/SplashActivity.java
@@ -15,9 +15,9 @@ import com.quickblox.sample.chat.R;
 
 public class SplashActivity extends Activity implements QBCallback {
 
-    private static final String APP_ID = "99";
-    private static final String AUTH_KEY = "63ebrp5VZt7qTOv";
-    private static final String AUTH_SECRET = "YavMAxm5T59-BRw";
+    private static final String APP_ID = "14818";
+    private static final String AUTH_KEY = "6zTZ8BF4RXVhpNK";
+    private static final String AUTH_SECRET = "SK4GwLAffgDSTd3";
 
     private ProgressBar progressBar;
 
